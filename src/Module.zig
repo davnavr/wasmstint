d9bae39ea523395bdbfbca4848424a4a352283f6
David N <davidcalieln@gmail.com>
2025-01-17 21:29:11 -0800

    feat(bin): add export, start, and code entry parser

--- src/Module.zig
+++ src/Module.zig
@@ -8,11 +8,64 @@ const Module = @This();
 pub const ValType = @import("Module/val_type.zig").ValType;
 pub const FuncType = @import("Module/FuncType.zig");
 
+pub const TypeIdx = enum(u31) {
+    _,
+};
+
+pub const FuncIdx = enum(u31) {
+    _,
+
+    pub fn signature(idx: FuncIdx, module: *const Module) *const FuncType {
+        return module.func_types.ptrAt(@intFromEnum(idx), module.data);
+    }
+};
+
 wasm: []const u8,
 types: IndexedArena.Slice(FuncType) = .empty,
 func_types: IndexedArena.Slice(IndexedArena.Idx(FuncType)) = .empty,
+code: struct {
+    section_start: [*]const u8 = undefined,
+    entries: IndexedArena.Slice(Code) = .empty,
+} = .{},
+imports_start: [*]const u8 = undefined,
+func_imports: IndexedArena.Slice(ImportName) = .empty,
+exports: struct {
+    section_start: [*]const u8 = undefined,
+    descs: IndexedArena.Slice(Export) = .empty,
+} = .{},
 custom_sections: IndexedArena.Slice(CustomSection) = .empty,
+start: packed struct(u32) { exists: bool = false, idx: FuncIdx = undefined } = .{},
 data: IndexedArena.ConstData = &[0]IndexedArena.Word{},
+
+pub const ImportName = struct {
+    name_offset: u32,
+    name_len: u16,
+
+    module_offset: u32,
+    module_len: u16,
+
+    //pub inline fn name()
+};
+
+pub const Export = struct {
+    name_offset: u32,
+    name_len: u32,
+    desc: Desc,
+
+    pub const Desc = union(enum(u2)) {
+        func: FuncIdx,
+    };
+
+    pub inline fn name(self: Export, module: *const Module) std.unicode.Utf8View {
+        return .{ .bytes = module.exports.section_start[self.name_offset .. self.name_offset + self.name_len] };
+    }
+};
+
+pub const Code = struct {
+    offset: u32,
+    size: u32,
+    // TODO: Ptr to side table
+};
 
 pub const CustomSection = struct {
     name_ptr: [*]const u8,
@@ -57,6 +110,12 @@ pub const LimitError = error{
     /// See <https://webassembly.github.io/spec/core/appendix/implementation.html>.
     WasmImplementationLimit,
 };
+
+pub const ParseError = error{
+    /// The input did not start with the WebAssembly preamble.
+    NotWasm,
+    InvalidWasm,
+} || ReaderError || LimitError || Allocator.Error;
 
 const Reader = struct {
     bytes: *[]const u8,
@@ -137,13 +196,15 @@ const Reader = struct {
 
         return reader.readByteTag(ValType);
     }
-};
 
-pub const ParseError = error{
-    /// The input did not start with the WebAssembly preamble.
-    NotWasm,
-    InvalidWasm,
-} || ReaderError || LimitError || Allocator.Error;
+    fn readIdx(reader: Reader, comptime I: type, slice: anytype) ParseError!I {
+        const idx = try reader.readUleb128(u32);
+        return if (idx < slice.len)
+            @enumFromInt(std.math.cast(@typeInfo(I).@"enum".tag_type, idx) orelse return error.WasmImplementationLimit)
+        else
+            error.InvalidWasm;
+    }
+};
 
 pub const ParseOptions = struct {
     /// If true, module data is initially allocated in a `scratch` allocator as it is resized.
@@ -166,6 +227,7 @@ pub fn parse(
     allocator: Allocator,
     wasm: *[]const u8,
     scratch: Allocator,
+    rng: std.Random,
     options: ParseOptions,
 ) ParseError!Module {
     var module = Module{ .wasm = wasm.* };
@@ -325,27 +387,55 @@ pub fn parse(
         known_sections.type = undefined;
     }
 
+    // Allocated in `temporary`.
     var func_import_types = std.SegmentedList(IndexedArena.Idx(FuncType), 8){};
 
     if (known_sections.import.len > 0) {
         const import_reader = Reader.init(&known_sections.import);
         errdefer wasm.* = import_reader.bytes.*;
         const import_len = try import_reader.readUleb128(u32);
-        // const imports
-        // module.
-        for (0..import_len) |i| {
+        module.imports_start = import_reader.bytes.*.ptr;
+
+        var names_arena = ArenaAllocator.init(temporary.allocator());
+        // Reserve space for all of the names.
+        _ = try names_arena.allocator().alloc(
+            ImportName,
+            std.math.add(usize, import_len, 2) catch return error.OutOfMemory,
+        );
+
+        _ = names_arena.reset(.retain_capacity);
+        var func_imports = std.SegmentedList(ImportName, 8){};
+
+        for (0..import_len) |_| {
             const mod = try import_reader.readName();
             const name = try import_reader.readName();
-            _ = i;
-            _ = mod;
-            _ = name;
+            const import_name = ImportName{
+                .module_offset = @intCast(@intFromPtr(mod.bytes.ptr) - @intFromPtr(module.imports_start)),
+                .module_len = std.math.cast(u16, mod.bytes.len) orelse return error.WasmImplementationLimit,
+
+                .name_offset = @intCast(@intFromPtr(name.bytes.ptr) - @intFromPtr(module.imports_start)),
+                .name_len = std.math.cast(u16, name.bytes.len) orelse return error.WasmImplementationLimit,
+            };
+
+            switch (try import_reader.readByteTag(ImportExportDesc)) {
+                .func => {
+                    try func_imports.append(names_arena.allocator(), import_name);
+                    try func_import_types.append(
+                        temporary.allocator(),
+                        try import_reader.readIdx(TypeIdx, module.types),
+                    );
+                },
+                else => unreachable, // TODO
+            }
         }
+
+        module.func_imports = try arena.dupeSegmentedList(ImportName, 8, &func_imports);
 
         try import_reader.expectEndOfStream();
         known_sections.import = undefined;
     }
 
-    if (known_sections.func.len > 0) {
+    const func_sec_len: u32 = if (known_sections.func.len > 0) len: {
         const func_reader = Reader.init(&known_sections.func);
         errdefer wasm.* = func_reader.bytes.*;
         const func_len = try func_reader.readUleb128(u32);
@@ -355,6 +445,7 @@ pub fn parse(
             std.math.add(u32, func_import_len, func_len) catch return error.InvalidWasm,
         );
 
+        // Cannot use `temporary` until this runs.
         func_import_types.writeToSlice(module.func_types.items(&arena)[0..func_import_len], 0);
 
         for (func_import_len..(func_import_len + func_len)) |i| {
@@ -364,16 +455,114 @@ pub fn parse(
 
         try func_reader.expectEndOfStream();
         known_sections.func = undefined;
-    } else {
+        break :len func_len;
+    } else no_func: {
+        // Cannot use `temporary` until this runs.
         module.func_types = try arena.dupeSegmentedList(
             IndexedArena.Idx(FuncType),
             8,
             &func_import_types,
         );
-    }
+        break :no_func 0;
+    };
 
     std.debug.assert(module.func_types.len >= func_import_types.len);
     func_import_types = undefined;
+
+    if (known_sections.@"export".len > 0) {
+        const export_reader = Reader.init(&known_sections.@"export");
+        errdefer wasm.* = export_reader.bytes.*;
+        const export_len = try export_reader.readUleb128(u32);
+
+        module.exports = .{
+            .section_start = export_reader.bytes.*.ptr,
+            .descs = try arena.alloc(Export, export_len),
+        };
+
+        const ExportDedupContext = struct {
+            seed: u64,
+
+            pub fn eql(_: @This(), a: []const u8, b: []const u8) bool {
+                return std.mem.eql(u8, a, b);
+            }
+
+            pub fn hash(ctx: @This(), name: []const u8) u64 {
+                return std.hash.Wyhash.hash(ctx.seed, name);
+            }
+        };
+
+        var export_dedup = std.HashMapUnmanaged(
+            []const u8,
+            void,
+            ExportDedupContext,
+            std.hash_map.default_max_load_percentage,
+        ).empty;
+
+        const export_dedup_context = ExportDedupContext{ .seed = rng.int(u64) };
+
+        _ = temporary.reset(.retain_capacity);
+        comptime std.debug.assert(std.hash_map.default_max_load_percentage > 75);
+        try export_dedup.ensureTotalCapacityContext(
+            temporary.allocator(),
+            // Prevent regrowth even with high load factor.
+            std.math.add(usize, export_len, export_len / 4) catch return error.OutOfMemory,
+            export_dedup_context,
+        );
+
+        for (module.exports.descs.items(&arena)) |*ex| {
+            const name = try export_reader.readName();
+            if (export_dedup.getOrPutAssumeCapacityContext(name.bytes, export_dedup_context).found_existing)
+                return ParseError.InvalidWasm;
+
+            ex.* = .{
+                .name_len = @intCast(name.bytes.len),
+                .name_offset = @intCast(@intFromPtr(name.bytes.ptr) - @intFromPtr(module.exports.section_start)),
+                .desc = switch (try export_reader.readByteTag(ImportExportDesc)) {
+                    .func => try export_reader.readIdx(FuncIdx, module.func_types),
+                    else => unreachable, // TODO
+                },
+            };
+        }
+
+        try export_reader.expectEndOfStream();
+        known_sections.@"export" = undefined;
+    }
+
+    if (known_sections.start.len > 0) {
+        const start_reader = Reader.init(&known_sections.start);
+        errdefer wasm.* = start_reader.bytes.*;
+
+        module.start = .{
+            .idx = try start_reader.readIdx(FuncIdx, module.func_types),
+            .exists = true,
+        };
+
+        try start_reader.expectEndOfStream();
+        known_sections.start = undefined;
+    }
+
+    if (known_sections.code.len > 0) {
+        const code_reader = Reader.init(&known_sections.code);
+        errdefer wasm.* = code_reader.bytes.*;
+        const code_len = try code_reader.readUleb128(u32);
+        if (code_len != func_sec_len) return error.MalformedWasm;
+
+        module.code = .{
+            .section_start = code_reader.bytes.*.ptr,
+            .entries = try arena.alloc(Code, code_len),
+        };
+
+        for (module.code.entries.items(&arena)) |*code| {
+            const contents = try code_reader.readByteVec();
+            code.* = .{
+                .size = @intCast(contents.len),
+                .offset = @intCast(@intFromPtr(contents.ptr) - @intFromPtr(module.code.section_start)),
+            };
+        }
+
+        try code_reader.expectEndOfStream();
+        known_sections.code = undefined;
+    } else if (func_sec_len > 0) return error.MalformedWasm;
 
     module.data = if (options.realloc_contents) realloc: {
         const src = arena.data.items;
