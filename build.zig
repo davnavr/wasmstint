e4a1ebe394af0afab257fce09025cd462a16148a
David N <davidcalieln@gmail.com>
2025-02-06 00:59:41 -0800

    test(spec): add binary.wast to passing list

--- build.zig
+++ build.zig
@@ -66,6 +66,7 @@ pub fn build(b: *std.Build) void {
             "tests/spec/address.wast",
             "tests/spec/align.wast",
             "tests/spec/binary-leb128.wast",
+            "tests/spec/binary.wast",
             "tests/spec/i32.wast",
             "tests/spec/i64.wast",
             "tests/spec/fac.wast",
