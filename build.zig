6f8c25752e2a9afcd79d05f37d8965d014652b8e
David N <davidcalieln@gmail.com>
2025-07-11 22:41:17 -0700

    build(spec): step to run wast2json on testsuite

--- build.zig
+++ build.zig
@@ -1,5 +1,4 @@
 const std = @import("std");
-const OomError = std.mem.Allocator.Error;
 const Build = std.Build;
 const Step = Build.Step;
 
@@ -45,14 +44,14 @@ const ProjectOptions = struct {
     }
 };
 
-const PathOptions = struct {
+const ToolPaths = struct {
+    wast2json: []const u8,
     // cargo: []const u8,
     // @"afl-clang-lto": []const u8,
 
-    fn init(b: *Build) PathOptions {
-        var options: PathOptions = undefined;
-
-        inline for (@typeInfo(PathOptions).@"struct".fields) |field| {
+    fn init(b: *Build) ToolPaths {
+        var options: ToolPaths = undefined;
+        inline for (@typeInfo(ToolPaths).@"struct".fields) |field| {
             @field(options, field.name) = b.option(
                 []const u8,
                 field.name,
@@ -85,19 +84,24 @@ const WasmstintModule = struct {
     }
 };
 
-pub fn build(b: *Build) OomError!void {
+pub fn build(b: *Build) void {
     const project_options = ProjectOptions.init(b);
-    // const path_options = PathOptions.init(b);
+    const tool_paths = ToolPaths.init(b);
 
     const steps = .{
         // .check = b.step("check", "Check for compilation errors"),
+
+        .install_spectest = b.step(
+            "install-spectest",
+            "Translate specification tests with wast2json",
+        ),
 
         .run_wast = b.step("run-wast", "Run the specification test interpreter"),
         // .run_wasip1 = b.step("run-wasip1", "Run the WASI (preview 1) application interpreter",),
 
         .@"test" = b.step("test", "Run all unit and specification tests"),
         .test_unit = b.step("test-unit", "Run unit tests"),
-        .test_spec = b.step("test-spec", "Run some specification tests"),
+        // .test_spec = b.step("test-spec", "Run some specification tests"),
     };
 
     const wasmstint_module = WasmstintModule.build(b, &project_options);
@@ -131,118 +135,98 @@ pub fn build(b: *Build) OomError!void {
     };
 
     const unit_tests = b.addTest(.{ .root_module = wasmstint_module.module });
-
     const unit_tests_run = b.addRunArtifact(unit_tests);
     steps.test_unit.dependOn(&unit_tests_run.step);
-
-    {
-        const run_spec_tests_cmd = b.addRunArtifact(executables.run_wast);
-        const tests = [_][]const u8{
-            "tests/spec/address.wast",
-            "tests/spec/align.wast",
-            "tests/spec/binary-leb128.wast",
-            "tests/spec/binary.wast",
-            "tests/spec/block.wast",
-            "tests/spec/br_if.wast",
-            "tests/spec/br_table.wast",
-            "tests/spec/br.wast",
-            "tests/spec/bulk.wast",
-            "tests/spec/call_indirect.wast",
-            "tests/spec/call.wast",
-            // "tests/spec/comment.wast", // TODO: pending support for module fields as abbreviation for module
-            // "tests/spec/const.wast", // TODO: need to fix float literal parsing
-            "tests/spec/conversions.wast",
-            "tests/spec/custom.wast",
-            "tests/spec/data.wast",
-            "tests/spec/elem.wast",
-            "tests/spec/endianness.wast",
-            "tests/spec/exports.wast",
-            "tests/spec/f32_bitwise.wast",
-            "tests/spec/f32_cmp.wast",
-            "tests/spec/f32.wast",
-            "tests/spec/f64_bitwise.wast",
-            "tests/spec/f64_cmp.wast",
-            "tests/spec/f64.wast",
-            "tests/spec/fac.wast",
-            "tests/spec/float_exprs.wast",
-            "tests/spec/float_literals.wast",
-            "tests/spec/float_memory.wast",
-            "tests/spec/float_misc.wast",
-            "tests/spec/forward.wast",
-            "tests/spec/func_ptrs.wast",
-            "tests/spec/func.wast",
-            "tests/spec/global.wast",
-            "tests/spec/i32.wast",
-            "tests/spec/i64.wast",
-            "tests/spec/if.wast",
-            "tests/spec/imports.wast",
-            // "tests/spec/inline-module.wast", // TODO: parser doesn't support inline modules
-            "tests/spec/int_exprs.wast",
-            "tests/spec/int_literals.wast",
-            "tests/spec/labels.wast",
-            "tests/spec/left-to-right.wast",
-            "tests/spec/linking.wast",
-            "tests/spec/load.wast",
-            "tests/spec/local_get.wast",
-            "tests/spec/local_set.wast",
-            "tests/spec/local_tee.wast",
-            "tests/spec/loop.wast",
-            "tests/spec/memory_copy.wast",
-            "tests/spec/memory_fill.wast",
-            "tests/spec/memory_grow.wast",
-            "tests/spec/memory_init.wast",
-            "tests/spec/memory_redundancy.wast",
-            "tests/spec/memory_size.wast",
-            "tests/spec/memory_trap.wast",
-            "tests/spec/memory.wast",
-            "tests/spec/names.wast",
-            "tests/spec/nop.wast",
-            "tests/spec/obsolete-keywords.wast", // Currently skipped
-            "tests/spec/ref_func.wast",
-            "tests/spec/ref_is_null.wast",
-            "tests/spec/ref_null.wast",
-            "tests/spec/return.wast",
-            "tests/spec/select.wast",
-
-            // simd_*.wast tests are skipped as 128-bit SIMD is not yet supported
-
-            "tests/spec/skip-stack-guard-page.wast",
-            "tests/spec/stack.wast",
-            "tests/spec/start.wast",
-            "tests/spec/store.wast",
-            "tests/spec/switch.wast",
-            "tests/spec/table_copy.wast",
-            "tests/spec/table_fill.wast",
-            "tests/spec/table_get.wast",
-            "tests/spec/table_set.wast",
-            "tests/spec/table_size.wast",
-            "tests/spec/table-sub.wast", // Currently skipped
-            "tests/spec/table.wast",
-            "tests/spec/token.wast",
-            "tests/spec/traps.wast",
-            "tests/spec/type.wast",
-            "tests/spec/unreachable.wast",
-            "tests/spec/unreached-invalid.wast", // Currently skipped
-            "tests/spec/unreached-valid.wast",
-            "tests/spec/unwind.wast",
-            "tests/spec/utf8-custom-section-id.wast", // Currently skipped
-            "tests/spec/utf8-import-field.wast", // Currently skipped
-            "tests/spec/utf8-import-module.wast", // Currently skipped
-            "tests/spec/utf8-invalid-encoding.wast", // Currently skipped
-        };
-
-        for (tests) |path| {
-            run_spec_tests_cmd.addArg("--run");
-            run_spec_tests_cmd.addFileArg(b.path(path));
-        }
-
-        if (b.args) |args| {
-            run_spec_tests_cmd.addArgs(args);
-        }
-
-        steps.test_spec.dependOn(&run_spec_tests_cmd.step);
-    }
-
     steps.@"test".dependOn(steps.test_unit);
-    steps.@"test".dependOn(steps.test_spec);
+
+    _ = executables;
+
+    const translate_spectests = TranslateSpectests.build(b, steps.install_spectest, &tool_paths);
+
+    _ = translate_spectests;
+    // steps.@"test".dependOn(steps.test_spec);
 }
+
+const TranslateSpectests = struct {
+    translate_step: *Step,
+    tests: []const Test,
+
+    const Test = struct {
+        name: []const u8,
+        json_path: Build.LazyPath,
+    };
+
+    fn build(
+        b: *Build,
+        install_step: *Step,
+        tool_paths: *const ToolPaths,
+    ) TranslateSpectests {
+        var spectests = std.ArrayListUnmanaged(Test).initCapacity(b.allocator, 147) catch
+            @panic("OOM");
+
+        const tests_dir = b.path("tests/spec");
+        const tests_dir_handle = b.build_root.handle.openDir(
+            tests_dir.src_path.sub_path,
+            .{ .iterate = true },
+        ) catch @panic("could not open tests directory");
+
+        var translate_step = b.allocator.create(Step) catch @panic("OOM");
+        translate_step.* = Step.init(.{
+            .id = .custom,
+            .name = @typeName(@This()),
+            .owner = b,
+        });
+        const translate_output = b.addWriteFiles();
+        const translate_output_dir = translate_output.getDirectory();
+        translate_step.dependOn(&translate_output.step);
+
+        var tests_iter = tests_dir_handle.iterateAssumeFirstIteration();
+        while (tests_iter.next() catch @panic("bad entry in tests directory")) |tests_entry| {
+            if (tests_entry.kind != .file or
+                !std.mem.eql(u8, ".wast", std.fs.path.extension(tests_entry.name)) or
+                std.mem.startsWith(u8, tests_entry.name, "simd_"))
+            {
+                continue;
+            }
+
+            var wast2json = b.addSystemCommand(&.{tool_paths.wast2json});
+            translate_step.dependOn(&wast2json.step);
+            wast2json.setCwd(translate_output_dir);
+            wast2json.addFileArg(tests_dir.path(b, tests_entry.name));
+
+            const name = b.allocator.dupe(u8, tests_entry.name[0 .. tests_entry.name.len - 5]) catch
+                @panic("OOM");
+
+            const json_name = b.fmt("{s}.json", .{name});
+            wast2json.addArgs(&.{ "--output", json_name });
+            // addPrefixedFileArg would mean each .json is in a separate dir
+            const json_path = translate_output_dir.path(b, json_name);
+
+            spectests.append(b.allocator, .{ .name = name, .json_path = json_path }) catch
+                @panic("OOM");
+        }
+
+        const step = if (spectests.items.len == 0)
+            &b.addFail("no .wast files found in test directory").step
+        else
+            translate_step;
+
+        install_step.dependOn(step);
+
+        if (spectests.items.len > 0) {
+            const install_spectests = b.addInstallDirectory(.{
+                .source_dir = translate_output_dir,
+                .install_dir = .{ .custom = "spectest" },
+                .install_subdir = ".",
+            });
+
+            install_spectests.step.dependOn(translate_step);
+            install_step.dependOn(&install_spectests.step);
+        }
+
+        return .{
+            .translate_step = step,
+            .tests = spectests.items,
+        };
+    }
+};
