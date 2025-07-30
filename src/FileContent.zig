b364ca7044d548bca9ddc3fb2d781a031d7016f9
David N <davidcalieln@gmail.com>
2025-07-30 14:41:16 -0700

    feat: FileContent allocation on windows

--- src/FileContent.zig
+++ src/FileContent.zig
@@ -3,11 +3,35 @@
 const FileContent = @This();
 
 contents: []align(page_size_min) const u8,
-/// On windows, all pages belonging to the same "allocation" must be freed
-/// at once, and cannot be partially freed.
-allocated_size: if (builtin.os.tag == .windows) usize else void,
 
 const ReadError = Oom || fs.File.OpenError || fs.File.ReadError || fs.File.StatError;
+
+fn calculateAllocationSizes(
+    page_size: usize,
+    allocated: []align(page_size_min) u8,
+    actual_size: usize,
+) struct { unused: []align(page_size_min) u8, used: []align(page_size_min) u8 } {
+    const unused_pages_size = std.mem.alignBackward(
+        usize,
+        allocated.len - actual_size,
+        page_size,
+    );
+
+    std.debug.assert(actual_size + unused_pages_size <= allocated.len);
+    std.debug.assert(unused_pages_size % page_size == 0);
+
+    const unused_pages_start_offset = allocated.len - unused_pages_size;
+    const unused_pages: []align(page_size_min) u8 = @alignCast(
+        allocated[unused_pages_start_offset .. unused_pages_start_offset + unused_pages_size],
+    );
+
+    const used_pages = allocated[0..std.mem.alignForward(usize, actual_size, page_size)];
+    std.debug.assert(
+        @intFromPtr(used_pages.ptr + used_pages.len) <= @intFromPtr(unused_pages.ptr),
+    );
+
+    return .{ .unused = unused_pages, .used = used_pages };
+}
 
 pub fn readFileZ(dir: Dir, path: [:0]const u8) ReadError!FileContent {
     const file = try dir.openFileZ(path, .{ .mode = .read_only });
@@ -29,7 +53,37 @@ pub fn readFileZ(dir: Dir, path: [:0]const u8) ReadError!FileContent {
     std.debug.assert(allocated_size % page_size == 0);
 
     if (builtin.os.tag == .windows) {
-        @panic("TODO: Windows VirtualAlloc(reserve and allocate) and then decommit");
+        const reserved = windows.VirtualAlloc(
+            null,
+            allocated_size,
+            windows.MEM_COMMIT | windows.MEM_RESERVE,
+            windows.PAGE_READWRITE,
+        ) catch return Oom.OutOfMemory;
+        errdefer windows.VirtualFree(reserved, 0, windows.MEM_RELEASE);
+
+        const allocated: []align(page_size_min) u8 =
+            @as([*]align(page_size_min) u8, @alignCast(@ptrCast(reserved)))[0..allocated_size];
+
+        const actual_size = try file.readAll(allocated);
+        const pages = calculateAllocationSizes(page_size, allocated, actual_size);
+
+        if (pages.unused.len > 0) {
+            windows.VirtualFree(
+                @ptrCast(pages.unused.ptr),
+                pages.unused.len,
+                windows.MEM_DECOMMIT,
+            );
+        }
+
+        var dummy_old_protect: windows.DWORD = undefined;
+        windows.VirtualProtect(
+            @ptrCast(pages.used.ptr),
+            pages.used.len,
+            windows.PAGE_READONLY,
+            &dummy_old_protect,
+        ) catch unreachable;
+
+        return .{ .contents = allocated[0..actual_size] };
     } else {
         // see `PageAllocator.map`
         const hint = @atomicLoad(
@@ -49,26 +103,10 @@ pub fn readFileZ(dir: Dir, path: [:0]const u8) ReadError!FileContent {
         errdefer posix.munmap(allocated);
 
         const actual_size = try file.readAll(allocated);
-        const unused_pages_size = std.mem.alignBackward(
-            usize,
-            allocated_size - actual_size,
-            page_size,
-        );
+        const pages = calculateAllocationSizes(page_size, allocated, actual_size);
 
-        std.debug.assert(actual_size + unused_pages_size <= allocated_size);
-        std.debug.assert(unused_pages_size % page_size == 0);
-
-        const unused_pages_start_offset = allocated_size - unused_pages_size;
-        const unused_pages: []align(page_size_min) const u8 = @alignCast(
-            allocated[unused_pages_start_offset .. unused_pages_start_offset + unused_pages_size],
-        );
-        const used_pages = allocated[0..std.mem.alignForward(usize, actual_size, page_size)];
-        std.debug.assert(
-            @intFromPtr(used_pages.ptr + used_pages.len) <= @intFromPtr(unused_pages.ptr),
-        );
-
-        if (unused_pages_size > 0) {
-            posix.munmap(unused_pages); // Does Zig allow unmapping some pages at the end?
+        if (pages.unused.len > 0) {
+            posix.munmap(pages.unused); // Does Zig allow unmapping some pages at the end?
         }
 
         // see `PageAllocator.map`
@@ -76,22 +114,23 @@ pub fn readFileZ(dir: Dir, path: [:0]const u8) ReadError!FileContent {
             @TypeOf(std.heap.next_mmap_addr_hint),
             &std.heap.next_mmap_addr_hint,
             hint,
-            @constCast(unused_pages.ptr),
+            @constCast(pages.unused.ptr),
             .monotonic,
             .monotonic,
         );
 
-        posix.mprotect(used_pages, posix.system.PROT.READ) catch |e| switch (e) {
+        posix.mprotect(pages.used, posix.system.PROT.READ) catch |e| switch (e) {
             // OOM probably shouldn't occur, since "whole" allocation is marked read-only
             error.OutOfMemory => |oom| return oom,
             else => unreachable,
         };
 
-        return .{ .contents = allocated[0..actual_size], .allocated_size = {} };
+        return .{ .contents = allocated[0..actual_size] };
     }
 }
 
 // pub fn deinit(contents: *FileContent) void {
+//     // Should instead use posix.unmap and windows.VirtualFree
 //     std.heap.PageAllocator.unmap(
 //         @alignCast(@constCast(contents.contents.ptr[0..contents.allocated_size])),
 //     );
@@ -100,6 +139,7 @@ pub fn readFileZ(dir: Dir, path: [:0]const u8) ReadError!FileContent {
 const std = @import("std");
 const builtin = @import("builtin");
 const posix = std.posix;
+const windows = std.os.windows;
 const Oom = std.mem.Allocator.Error;
 const pageSize = std.heap.pageSize;
 const page_size_min = std.heap.page_size_min;
