ed273bf6829ee38082f42804e2671b0ce41a8989
David N <davidcalieln@gmail.com>
2025-08-01 23:17:00 -0700

    feat(interp): grow helper for paged_memory

--- src/runtime/paged_memory.zig
+++ src/runtime/paged_memory.zig
@@ -4,7 +4,6 @@ const MapError = ModuleAllocating.LimitsError || Oom;
 
 /// Allocates OS memory pages for use as a `MemInst`.
 ///
-///
 /// Asserts that `initial_size <= initial_capacity`.
 pub fn map(
     mem_type: *const MemType,
@@ -15,25 +14,26 @@ pub fn map(
     ///
     /// If below `mem_type.limits.min`, then `error.LimitsMismatch` is returned.
     initial_capacity: usize,
-    /// The initial total size of the memory allocation, in bytes, rounded down to the nearest
+    /// The maximum size of the memory allocation, in bytes, rounded down to the nearest
     /// multiple of `MemInst.page_size`.
     ///
     /// On Windows, this is the amount of memory reserved.
-    initial_reserve: usize,
+    /// On Unix-like platforms, pages with the `PROT_NONE` flag are mapped instead.
+    maximum_size: usize,
 ) MapError!MemInst {
     comptime {
         std.debug.assert(page_size_min <= MemInst.page_size);
     }
 
     std.debug.assert(mem_type.limits.min <= mem_type.limits.max);
-    std.debug.assert(initial_capacity <= initial_reserve);
+    std.debug.assert(initial_capacity <= maximum_size);
     const min_bytes = @as(u32, @intCast(mem_type.limits.min)) * MemInst.page_size;
     const max_bytes = @as(u32, @intCast(mem_type.limits.max)) * MemInst.page_size;
 
     const reserve: u32 = @intCast(
         @min(
             max_bytes,
-            std.mem.alignBackward(usize, initial_reserve, MemInst.page_size),
+            std.mem.alignBackward(usize, maximum_size, MemInst.page_size),
         ),
     );
 
@@ -88,6 +88,8 @@ pub fn map(
         );
         errdefer posix.munmap(pages);
 
+        // This recreates windows commit behavior
+        // TODO: Linux has mremap so only other Unix targets are required to do this
         if (!single_syscall) {
             posix.mprotect(pages[0..capacity], posix.system.PROT.WRITE) catch |e| switch (e) {
                 error.OutOfMemory => |oom| return oom,
@@ -125,33 +127,74 @@ pub fn map(
 pub fn allocate(
     request: *ModuleAllocating,
     initial_capacity: usize,
-    initial_reserve: usize,
+    maximum: usize,
 ) MapError!void {
-    const mem_inst = try map(request.nextMemoryType().?, initial_capacity, initial_reserve);
+    const mem_inst = try map(request.nextMemoryType().?, initial_capacity, maximum);
     request.nextMemory().* = mem_inst;
 }
 
-pub fn free(mem: *MemInst) void {
+fn checkMemInst(mem: *const MemInst) void {
     if (builtin.mode == .Debug) {
         const page_size = pageSize();
         std.debug.assert(mem.size % page_size == 0);
         std.debug.assert(mem.capacity % page_size == 0);
         std.debug.assert(mem.limit % page_size == 0);
+        // Checking dwAllocationGranularity on Windows is overkill
+        std.debug.assert(@intFromPtr(mem.base) % page_size == 0);
     }
+}
 
+pub fn free(mem: *MemInst) void {
+    checkMemInst(mem);
     if (builtin.os.tag == .windows) {
-        if (builtin.mode == .Debug) {
-            var info: windows.SYSTEM_INFO = undefined;
-            windows.kernel32.GetSystemInfo(&info);
-            std.debug.assert(@intFromPtr(mem.base) % info.dwAllocationGranularity == 0);
-        }
-
         windows.VirtualFree(@ptrCast(mem.base), 0, windows.MEM_RELEASE);
     } else {
         posix.munmap(@alignCast(mem.bytes()));
     }
 
     mem.* = undefined;
+}
+
+/// Fulfills a growth request for the given memory instance allocated in OS virtual memory pages.
+///
+/// On Windows, new pages are committed from the existing allocation.
+/// On Unix-like platforms, `mprotect()` is called to allow access to the new pages.
+///
+/// Because `MemInst.limit` tracks both the memory's maximum and the current size of
+/// the allocation, `mremap` is unable to be used on Linux without introducing a
+/// new `usize` value.
+///
+/// TODO: do that ^
+pub fn grow(
+    request: *const Interpreter.InterruptionCause.MemoryGrow,
+) void {
+    checkMemInst(request.memory);
+    // No overflow, already handled by Interpreter's memory.grow handler
+    const new_bytes_size = request.memory.size + request.delta;
+    std.debug.assert(new_bytes_size <= request.memory.limit);
+    std.debug.assert(new_bytes_size % MemInst.buffer_align == 0);
+    // TODO: Track old size in MemoryGrow to allow make function idempotent?
+    std.debug.assert(request.memory.capacity < new_bytes_size);
+
+    const new_pages: []align(page_size_min) u8 = @alignCast(
+        request.memory.base[0..request.memory.limits][request.memory.size..new_bytes_size],
+    );
+
+    if (builtin.os.tag == .windows) {
+        windows.VirtualAlloc(
+            @ptrCast(new_pages),
+            new_pages.len,
+            windows.MEM_COMMIT,
+            windows.PAGE_READWRITE,
+        ) catch return;
+    } else {
+        posix.mprotect(new_pages, posix.PROT.READ | posix.PROT.WRITE) catch |e| switch (e) {
+            error.OutOfMemory => return,
+            error.AccessDenied, error.Unexpected => unreachable,
+        };
+    }
+
+    request.memory.capacity = new_bytes_size;
 }
 
 const std = @import("std");
@@ -164,3 +207,4 @@ const Oom = std.mem.Allocator.Error;
 const MemType = @import("../Module.zig").MemType;
 const ModuleAllocating = @import("ModuleAllocating.zig");
 const MemInst = @import("memory.zig").MemInst;
+const Interpreter = @import("../Interpreter.zig");
