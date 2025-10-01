8684c2b02844fed6f67cb7ae5a1769d2890ff793
David N <davidcalieln@gmail.com>
2025-10-01 16:03:10 -0700

    chore(wasip1): use preopen dir name API in ls.zig sample

--- tests/wasip1/zig/ls.zig
+++ tests/wasip1/zig/ls.zig
@@ -1,8 +1,31 @@
 pub fn main() !void {
-    var stdout_buf: [512]u8 align(16) = undefined;
+    var stdout_buf: [4096]u8 align(16) = undefined;
     var stdout_writer = std.fs.File.stdout().writerStreaming(&stdout_buf);
     const stdout = &stdout_writer.interface;
     defer stdout.flush() catch {};
+
+    const cwd = std.fs.cwd();
+    {
+        var prestat: wasi.prestat_t = undefined;
+        switch (wasi.fd_prestat_get(cwd.fd, &prestat)) {
+            .SUCCESS => {},
+            else => |err| return std.posix.unexpectedErrno(err),
+        }
+
+        const name_buf = try std.heap.wasm_allocator.alignedAlloc(
+            u8,
+            .@"16",
+            prestat.u.dir.pr_name_len,
+        );
+        // defer std.heap.wasm_allocator.free(name_buf);
+
+        switch (wasi.fd_prestat_dir_name(cwd.fd, name_buf.ptr, name_buf.len)) {
+            .SUCCESS => {},
+            else => |err| return std.posix.unexpectedErrno(err),
+        }
+
+        try stdout.print("{s}\n", .{name_buf});
+    }
 
     const kind_width = std.fmt.comptimePrint("{[width]}", .{
         .width = comptime width: {
@@ -14,13 +37,15 @@ pub fn main() !void {
         },
     });
 
-    var iterator = std.fs.cwd().iterate();
+    var iterator = cwd.iterateAssumeFirstIteration();
+    try stdout.print("total {}\n", .{iterator.end_index - iterator.index});
     while (try iterator.next()) |entry| {
         try stdout.print(
-            "{[kind]t: >" ++ kind_width ++ "} {[name]f}\n",
-            .{ .kind = entry.kind, .name = std.unicode.fmtUtf8(entry.name) },
+            "{[kind]t: >" ++ kind_width ++ "} {[name]s}\n",
+            .{ .kind = entry.kind, .name = entry.name },
         );
     }
 }
 
 const std = @import("std");
+const wasi = std.os.wasi;
