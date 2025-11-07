715287b9be623810db054b47e64d67a3cf7955ea
David N <davidcalieln@gmail.com>
2025-11-07 13:53:41 -0800

    revert: "chore: UnrolledLinkedList to replace std.SegmentedList"
    This reverts commit dedc3a902db8aa2e58e477a7f839bef58f1b3fb0.

--- src/allocators.zig
+++ src/allocators.zig
@@ -6,7 +6,6 @@ pub const ReservationAllocator = reservation_allocator.ReservationAllocator;
 pub const ArenaFallbackAllocator = @import("allocators/ArenaFallbackAllocator.zig");
 pub const virtual_memory = @import("allocators/virtual_memory.zig");
 pub const PageAllocation = @import("allocators/PageAllocation.zig");
-pub const UnrolledLinkedList = @import("allocators/unrolled_linked_list.zig").UnrolledLinkedList;
 
 test {
     _ = LimitedAllocator;
@@ -14,5 +13,4 @@ test {
     _ = ArenaFallbackAllocator;
     _ = virtual_memory;
     _ = PageAllocation;
-    _ = UnrolledLinkedList;
 }
