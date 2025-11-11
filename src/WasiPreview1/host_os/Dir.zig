davnavr/wasmstint

commit 1623b1d8995e997570c5c939784c4c9f3a8b0471

refactor(wasip1): add missing Windows-specific code after cb5aa5
Finally properly implements `fd_readdir`.

src/WasiPreview1/host_os/Dir.zig
@@ -25,13 +25,90 @@ pub const OpenOptions = struct {
     access_sub_paths: bool = true,
     iterate: bool = true,
     follow_symlinks: bool = true,
-    interrupt_retry_count: u8 = 5,
+    interrupt_retry_count: u7 = 5,
 };
 
 /// Equivalent of `std.Io.Dir.openDir`.
 pub fn openDir(dir: Dir, path: Path, options: OpenOptions) OpenError!Dir {
+    const attempt_count = @as(u8, options.interrupt_retry_count) + 1;
     if (host_os.is_windows) {
-        @panic("TODO: see host_dir.zig on usage of OBJ_DONT_REPARSE w/ NtCreateFile");
+        if (host_os.windows.has_obj_dont_reparse != true) {
+            @compileError(std.fmt.comptimePrint(
+                "OBJ_DONT_REPARSE to handle !follow_symlinks requires windows version {t}, got {t}",
+                .{
+                    @tagName(host_os.windows.obj_dont_reparse_min_version),
+                    @tagName(builtin.os.version_range.windows.min),
+                },
+            ));
+        }
+
+        const base_access_flags = host_os.windows.AccessMask.init(&.{
+            .STANDARD_RIGHTS_READ,
+            .FILE_READ_ATTRIBUTES,
+            .FILE_READ_EA,
+            .SYNCHRONIZE,
+        });
+        const access_flags = base_access_flags
+            .setFlagConditional(options.access_sub_paths, .FILE_TRAVERSE)
+            .setFlagConditional(options.iterate, .FILE_LIST_DIRECTORY);
+
+        var opened: host_os.Handle = undefined;
+        var io: std.os.windows.IO_STATUS_BLOCK = undefined;
+        var object_name = host_os.windows.initUnicodeString(@constCast(path));
+        var attrs = std.os.windows.OBJECT_ATTRIBUTES{
+            .Length = @sizeOf(std.os.windows.OBJECT_ATTRIBUTES),
+            .RootDirectory = dir.handle,
+            // TODO: Try std.os.windows.removeDotDirsSanitized and look into proper converion of
+            // Win32 paths to NT paths
+            .ObjectName = &object_name,
+            .Attributes = if (options.follow_symlinks) 0 else host_os.windows.OBJ_DONT_REPARSE,
+            .SecurityDescriptor = null,
+            .SecurityQualityOfService = null,
+        };
+
+        for (0..attempt_count) |_| {
+            const status = host_os.windows.NtCreateFile(
+                &opened,
+                access_flags,
+                &attrs,
+                &io,
+                null,
+                host_os.windows.FileAttributes.init(&.{.FILE_ATTRIBUTE_NORMAL}),
+                host_os.windows.share_access_default,
+                host_os.windows.CreateDisposition.FILE_OPEN,
+                host_os.windows.CreateOptions.init(&.{
+                    .FILE_DIRECTORY_FILE,
+                    .FILE_SYNCHRONOUS_IO_NONALERT,
+                    .FILE_OPEN_FOR_BACKUP_INTENT,
+                }),
+                null,
+                0,
+            );
+
+            return switch (status) {
+                .SUCCESS => Dir{ .handle = opened },
+                .ACCESS_DENIED => error.AccessDenied,
+                host_os.windows.STATUS_REPARSE_POINT_ENCOUNTERED => if (options.follow_symlinks)
+                    unreachable
+                else
+                    error.SymLinkLoop,
+                .OBJECT_NAME_INVALID => error.BadPathName,
+                .OBJECT_NAME_NOT_FOUND, .OBJECT_PATH_NOT_FOUND => error.FileNotFound,
+                .BAD_NETWORK_PATH, .BAD_NETWORK_NAME => error.NetworkNotFound,
+                .DELETE_PENDING => {
+                    @branchHint(.cold);
+                    _ = std.os.windows.kernel32.SleepEx(
+                        42, // arbitrary amount
+                        std.os.windows.TRUE,
+                    );
+                    continue;
+                },
+                .VIRUS_INFECTED, .VIRUS_DELETED => error.AntivirusInterference,
+                else => std.os.windows.unexpectedStatus(status),
+            };
+        }
+
+        return error.Interrupted;
     } else {
         var flags = std.posix.O{
             .ACCMODE = .RDONLY,
@@ -44,7 +121,7 @@ pub fn openDir(dir: Dir, path: Path, options: OpenOptions) OpenError!Dir {
             flags.PATH = true;
         }
 
-        for (0..(@as(u9, options.interrupt_retry_count) + 1)) |_| {
+        for (0..attempt_count) |_| {
             const result = host_os.unix_like.openat(
                 dir.handle,
                 path,
@@ -53,7 +130,10 @@ pub fn openDir(dir: Dir, path: Path, options: OpenOptions) OpenError!Dir {
             );
             return switch (std.posix.errno(result)) {
                 .SUCCESS => return Dir{ .handle = @intCast(result) },
-                .INTR => continue,
+                .INTR => {
+                    @branchHint(.unlikely);
+                    continue;
+                },
                 .INVAL => error.BadPathName,
                 .ACCES => error.AccessDenied,
                 .LOOP => error.SymLinkLoop,
@@ -78,6 +158,7 @@ pub fn openDir(dir: Dir, path: Path, options: OpenOptions) OpenError!Dir {
 
 const has_dirent = @hasDecl(std.posix.system, "dirent") and std.posix.system.dirent != void;
 
+/// Does not include *any* null-terminators, if they are usually used in host OS APIs.
 pub const max_name_bytes = if (host_os.is_windows)
     std.os.windows.NAME_MAX * 2
 else
@@ -85,11 +166,13 @@ else
 
 const LinuxEntry = std.os.linux.dirent64;
 
+const WindowsEntry = host_os.windows.FILE_ID_FULL_DIR_INFORMATION;
+
 pub const Entry = if (builtin.link_libc and has_dirent)
     std.c.dirent
 else switch (builtin.os.tag) {
     .linux => LinuxEntry,
-    .windows => @compileError("TODO: windows directory entry type"),
+    .windows => WindowsEntry,
     else => |bad| @compileError("no dirent for " ++ @tagName(bad) ++ ", try linking libc"),
 };
 
@@ -99,17 +182,20 @@ pub const entry = struct {
     pub const NameLen = std.math.IntFittingRange(0, max_name_bytes);
 
     pub fn nameBytesLen(ent: *align(entry_align) const Entry) NameLen {
-        return switch (builtin.os.tag) {
-            .linux => @intCast(ent.reclen - @offsetOf(Entry, "name") - 1),
+        return @intCast(switch (builtin.os.tag) {
+            .linux => ent.reclen - @offsetOf(Entry, "name") - 1,
+            .windows => ent.FileNameLength,
             else => |bad| @compileError("name len of entry for " ++ @tagName(bad)),
-        };
+        });
     }
 
     pub fn name(ent: *align(entry_align) const Entry) host_os.Path {
-        return if (host_os.is_windows)
-            @compileError("TODO: name on windows")
-        else switch (builtin.os.tag) {
+        return switch (builtin.os.tag) {
             .linux => @as([*]const u8, @ptrCast(&ent.name))[0..nameBytesLen(ent) :0],
+            .windows => @as(
+                [*]const std.os.windows.WCHAR,
+                @ptrCast(&ent.FileName),
+            )[0..@divExact(nameBytesLen(ent), 2)],
             else => |bad| @compileError("name of entry for " ++ @tagName(bad)),
         };
     }
@@ -140,8 +226,7 @@ pub fn iterate(dir: Dir, buffer: []align(Iterator.buffer_align) u8) Iterator {
     };
 }
 
-/// Equivalent of `std.fs.Dir.Iterator`, except that it exposes the underlying OS-specific
-/// directory entry structures.
+/// Thin wrapper over OS directory iteration APIs, similar to `std.fs.Dir.Iterator`
 pub const Iterator = struct {
     pub const min_buffer_size = @sizeOf(Entry) + max_name_bytes;
 
@@ -153,15 +238,22 @@ pub const Iterator = struct {
     remaining: []align(entry_align) const u8,
 
     fn current(iter: *const Iterator) ?*align(entry_align) const Entry {
+        if (iter.remaining.len <= @sizeOf(Entry)) {
+            return null;
+        }
+
         switch (builtin.os.tag) {
             .linux => {
-                if (iter.remaining.len <= @sizeOf(LinuxEntry)) {
-                    return null;
-                }
-
                 const peeked_entry: *align(1) const LinuxEntry = @ptrCast(iter.remaining);
                 std.debug.assert(@sizeOf(LinuxEntry) <= peeked_entry.reclen);
                 return if (iter.remaining.len < peeked_entry.reclen) null else peeked_entry;
+            },
+            .windows => {
+                const peeked_entry: *align(2) const WindowsEntry = @ptrCast(iter.remaining);
+                return if (iter.remaining.len - @sizeOf(WindowsEntry) < peeked_entry.FileNameLength)
+                    null
+                else
+                    peeked_entry;
             },
             else => |bad| @compileError("peek current dir entry for " ++ @tagName(bad)),
         }
@@ -171,18 +263,47 @@ pub const Iterator = struct {
         AccessDenied,
     };
 
-    fn peekWindows(iter: *Iterator) Error!?*align(2) const Entry {
-        // if (iter.needs_reset) // Set flag in NtQueryDirectoryFile
-        _ = iter;
-        @panic("TODO: NtQueryDirectoryFile & FILE_DIRECTORY_INFORMATION + NtQueryDirectoryFileEx FILE_INTERNAL_INFORMATION on file name");
+    fn readWindows(iter: *Iterator) Error!?*align(2) const Entry {
+        std.debug.assert(iter.remaining.len == 0);
+        @memset(iter.buffer, undefined);
+        var io: std.os.windows.IO_STATUS_BLOCK = undefined;
+        const status = host_os.windows.queryDirectoryFile(
+            iter.dir.handle,
+            &io,
+            iter.buffer,
+            .FileIdFullDirectoryInformation,
+            if (iter.needs_reset) .restart else .@"resume",
+        );
+        return switch (status) {
+            .NO_MORE_FILES => null, // does this case every actually run?
+            .INVALID_INFO_CLASS => @panic("FileIdFullDirectoryInformation not supported"),
+            .SUCCESS => {
+                iter.needs_reset = false;
+                iter.remaining = iter.buffer[0..io.Information];
+                return if (io.Information > 0) @ptrCast(@alignCast(iter.remaining)) else null;
+            },
+            .BUFFER_OVERFLOW,
+            .BUFFER_TOO_SMALL,
+            => unreachable, // min buffer size prevents this
+            .ACCESS_DENIED => error.AccessDenied,
+            else => std.os.windows.unexpectedStatus(status),
+        };
     }
 
-    fn peekPosix(iter: *Iterator) Error!?*align(1) const Entry {
-        if (builtin.os.tag == .linux) {
-            if (iter.current()) |peeked_entry| {
-                return peeked_entry;
-            }
+    fn readPosix(iter: *Iterator) Error!?*align(1) const Entry {
+        std.debug.assert(iter.remaining.len == 0);
+        if (iter.needs_reset) {
+            @branchHint(.unlikely);
+            std.posix.lseek_SET(iter.dir.handle, 0) catch |e| switch (e) {
+                error.AccessDenied => unreachable, // directory should allow iteration
+                error.Unexpected => |err| return err,
+                error.Canceled => unreachable,
+                error.Unseekable => unreachable,
+            };
+            iter.needs_reset = false;
+        }
 
+        if (builtin.os.tag == .linux) {
             @memset(iter.buffer, 0);
             const result = std.os.linux.getdents64(
                 iter.dir.handle,
@@ -210,21 +331,15 @@ pub const Iterator = struct {
 
     /// May deinitialize any previous `Entry`s that were returned by `.next()`.
     pub fn peek(iter: *Iterator) Error!?*align(entry_align) const Entry {
-        if (host_os.is_windows) {
-            return iter.peekWindows();
-        } else {
-            if (iter.needs_reset) {
-                @branchHint(.unlikely);
-                std.posix.lseek_SET(iter.dir.handle, 0) catch |e| switch (e) {
-                    error.AccessDenied => unreachable, // directory should allow iteration
-                    error.Unexpected => |err| return err,
-                    error.Canceled => unreachable,
-                    error.Unseekable => unreachable,
-                };
-                iter.needs_reset = false;
-            }
+        if (iter.current()) |peeked_entry| {
+            return peeked_entry;
+        }
 
-            return iter.peekPosix();
+        iter.remaining = iter.buffer[0..0];
+        if (host_os.is_windows) {
+            return iter.readWindows();
+        } else {
+            return iter.readPosix();
         }
     }
 
@@ -235,11 +350,11 @@ pub const Iterator = struct {
         const ent = iter.current().?;
         const size = switch (builtin.os.tag) {
             .linux => ent.reclen,
-            else => |bad| @compileError(
-                "determine reclen for entry for implementation on " ++ @tagName(bad),
-            ),
+            .windows => if (ent.NextEntryOffset == 0) iter.remaining.len else ent.NextEntryOffset,
+            else => |bad| @compileError("directory entry reclen for " ++ @tagName(bad)),
         };
-        iter.remaining = iter.remaining[size..];
+
+        iter.remaining = @alignCast(iter.remaining[size..]);
     }
 
     pub fn next(iter: *Iterator) Error!?*align(entry_align) const Entry {

src/WasiPreview1/File.zig
@@ -272,7 +272,6 @@ pub const VTable = struct {
     fd_readdir: *const fn (
         ctx: Ctx,
         inode_hash_seed: types.INode.HashSeed,
-        // allocator: Allocator,
         buf: []u8,
         cookie: types.DirCookie,
     ) Error!types.Size,

src/WasiPreview1/File/host_dir.zig
@@ -222,7 +222,6 @@ const EntryBuf = struct {
 pub fn fd_readdir(
     ctx: Ctx,
     inode_hash_seed: types.INode.HashSeed,
-    // allocator: Allocator,
     buf: []u8,
     cookie: types.DirCookie,
 ) Error!types.Size {
@@ -265,16 +264,29 @@ pub fn fd_readdir(
         }
     }
 
+    const NameBuffer = if (host_os.is_windows) struct {
+        space: [std.os.windows.NAME_MAX]u8 align(16) = undefined,
+    } else void;
+
+    var name_buffer = if (host_os.is_windows) NameBuffer{};
+
     var current_cookie = cookie;
     defer self.info.read_next_cookie = current_cookie;
     var entries = EntryBuf{ .bytes = buf };
     while (entries.bytes.len > 0) {
         const next = (try self.info.read_state.peek()) orelse break;
-        // TODO: Need scratch allocator to convert windows WTF-16 name
-        const name = Path.init(host_os.Dir.entry.name(next)) catch |e| switch (e) {
+        const host_name = host_os.Dir.entry.name(next);
+        const wtf8_name = if (host_os.is_windows)
+            name_buffer.space[0..std.unicode.wtf16LeToWtf8(&name_buffer.space, host_name)]
+        else
+            host_name;
+
+        defer if (host_os.is_windows) @memset(&name_buffer.space, undefined);
+
+        const name = Path.init(wtf8_name) catch |e| switch (e) {
             error.PathTooLong => unreachable, // no supported OS's allow names this long
             // Could silently skip non-UTF-8 entries, but Zig `std` feels the need to catch it
-            error.InvalidUtf8 => |err| return err,
+            error.InvalidUtf8 => |err| if (host_os.is_windows) unreachable else return err,
         };
 
         errdefer comptime unreachable;
@@ -294,11 +306,22 @@ pub fn fd_readdir(
                 _,
                 => .unknown,
             },
+            .windows => win: {
+                const attr = next.FileAttributes;
+                break :win if (attr.containsFlag(.FILE_ATTRIBUTE_DIRECTORY))
+                    .directory
+                else if (attr.containsFlag(.FILE_ATTRIBUTE_NORMAL))
+                    .regular_file
+                else
+                    // TODO: See if windows detects FILE_ATTRIBUTE_REPARSE_POINT here
+                    .unknown;
+            },
             else => |bad| @compileError("dir entry type for " ++ @tagName(bad)),
         };
 
         const inode: u64 = switch (builtin.os.tag) {
             .linux => next.ino,
+            .windows => @bitCast(next.FileId),
             else => |bad| @compileError("dir entry inode for " ++ @tagName(bad)),
         };
 
@@ -346,13 +369,9 @@ fn path_create_directory(ctx: Ctx, path: []const u8) Error!void {
 const WindowsOpenFlags = struct {
     access_mask: host_os.windows.AccessMask,
     comptime file_attributes: std.os.windows.ULONG = std.os.windows.FILE_ATTRIBUTE_NORMAL,
-    share_access: host_os.windows.ShareAccess = share_access_default,
+    share_access: host_os.windows.ShareAccess = host_os.windows.share_access_default,
     create_disposition: host_os.windows.CreateDisposition,
     create_options: host_os.windows.CreateOptions,
-
-    const share_access_default = host_os.windows.ShareAccess.init(
-        &.{ .FILE_SHARE_READ, .FILE_SHARE_WRITE, .FILE_SHARE_DELETE },
-    );
 };
 
 const OsOpenFlags = if (builtin.os.tag == .windows)
@@ -595,19 +614,14 @@ fn accessSubPathPortable(
             else
                 try path_arena.allocator().dupeZ(u8, comp_bytes);
 
-            // TODO: Use O_PATH on Linux, requires using platform-specific APIs
-
             const open_options = host_os.Dir.OpenOptions{
                 .access_sub_paths = true,
-                // TODO(zig): no_follow weird on windows https://github.com/ziglang/zig/issues/18335
                 .follow_symlinks = false,
             };
 
             final_dir = old_dir.openDir(comp_str, open_options) catch |e| return switch (e) {
                 // error.NotDir might happen on windows because a symlink is obviously not a directory
-                error.SymLinkLoop => if (builtin.os.tag == .windows)
-                    unreachable
-                else if (!flags.symlink_follow)
+                error.SymLinkLoop => if (!flags.symlink_follow)
                     error.SymLinkLoop
                 else {
                     // readLink + std.path.isAbsolute
@@ -652,7 +666,7 @@ fn accessSubPathPortable(
             var new_fd: std.os.windows.HANDLE = undefined;
             const result = host_os.windows.NtDuplicateObject(
                 current_process,
-                final_dir.fd,
+                final_dir.handle,
                 current_process,
                 &new_fd,
                 // TODO: Figure out access denied error or use ReOpenFile
@@ -679,21 +693,10 @@ fn accessSubPathPortable(
 
         var final_name_unicode = host_os.windows.initUnicodeString(final_name_w);
 
-        // Documentation for `NtCreateFile` only lists `OBJ_CASE_INSENSITIVE` for `Attributes`
-        //
-        // `OBJ_DONT_REPARSE` fails on paths like `C:\Users\You\file.txt`, but this only needs to
-        // process relative paths.
-        //
-        // For more information see:
-        // https://www.tiraniddo.dev/2020/05/objdontreparse-is-mostly-useless.html
-        const obj_dont_reparse_min_version = std.Target.Os.WindowsVersion.win10_rs1;
-        const has_obj_dont_reparse =
-            builtin.os.version_range.windows.isAtLeast(obj_dont_reparse_min_version);
-
-        const new_fd: host_os.Handle = if (has_obj_dont_reparse == true) opened: {
+        const new_fd: host_os.Handle = if (host_os.windows.has_obj_dont_reparse == true) opened: {
             var attrs = std.os.windows.OBJECT_ATTRIBUTES{
                 .Length = @sizeOf(std.os.windows.OBJECT_ATTRIBUTES),
-                .RootDirectory = final_dir.fd,
+                .RootDirectory = final_dir.handle,
                 .ObjectName = &final_name_unicode,
                 .Attributes = host_os.windows.OBJ_DONT_REPARSE,
                 .SecurityDescriptor = null,
@@ -710,7 +713,7 @@ fn accessSubPathPortable(
                     &attrs,
                     &io,
                     null,
-                    std.os.windows.FILE_ATTRIBUTE_NORMAL,
+                    host_os.windows.FileAttributes.init(&.{.FILE_ATTRIBUTE_NORMAL}),
                     initial_flags.share_access,
                     initial_flags.create_disposition,
                     initial_flags.create_options,
@@ -732,6 +735,15 @@ fn accessSubPathPortable(
                     .OBJECT_NAME_INVALID => return error.BadPathName,
                     .OBJECT_NAME_NOT_FOUND, .OBJECT_PATH_NOT_FOUND => return error.FileNotFound,
                     .BAD_NETWORK_PATH, .BAD_NETWORK_NAME => return error.NetworkNotFound,
+                    .NOT_A_DIRECTORY => return error.NotDir,
+                    .DELETE_PENDING => {
+                        @branchHint(.cold);
+                        _ = std.os.windows.kernel32.SleepEx(
+                            42, // arbitrary amount
+                            std.os.windows.TRUE,
+                        );
+                        continue;
+                    },
                     else => return std.os.windows.unexpectedStatus(status),
                 }
             }
@@ -744,7 +756,7 @@ fn accessSubPathPortable(
                         "remove the version check.",
                     .{
                         .actual = builtin.os.version_range.windows.min,
-                        .expected = obj_dont_reparse_min_version,
+                        .expected = host_os.windows.obj_dont_reparse_min_version,
                         .cpu = builtin.cpu.arch,
                     },
                 ));

src/WasiPreview1/PreopenDir.zig
@@ -38,7 +38,7 @@ pub fn openAt(
 
     std.log.debug(
         "preopen host {any} @ {f} at guest path {f} -> host {any}",
-        .{ dir, std.unicode.fmtUtf8(sub_path), guest_path, opened_dir.handle },
+        .{ dir, host_os.path.fmt(sub_path), guest_path, opened_dir.handle },
     );
 
     return PreopenDir{

src/WasiPreview1/host_os/path.zig
@@ -23,6 +23,17 @@ pub fn allocFromBytesZ(path: [:0]const u8, mode: AllocMode, alloc: Allocator) Al
     };
 }
 
+pub fn format(path: Slice, writer: *std.Io.Writer) std.Io.Writer.Error!void {
+    try writer.print(
+        "{f}",
+        .{if (host_os.is_windows) std.unicode.fmtUtf16Le(path) else std.unicode.fmtUtf8(path)},
+    );
+}
+
+pub fn fmt(path: Slice) std.fmt.Alt(Slice, format) {
+    return .{ .data = path };
+}
+
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 const host_os = @import("../host_os.zig");

commit dec42d77edde376bed7f966dd3548b8e67c0e875

chore(wasip1): fix Dir.Iterator.peekLinux bug introduced in 1623b1d

src/WasiPreview1/host_os/Dir.zig
@@ -57,6 +57,7 @@ pub fn openDir(dir: Dir, path: Path, options: OpenOptions) OpenError!Dir {
         var object_name = host_os.windows.initUnicodeString(@constCast(path));
         var attrs = std.os.windows.OBJECT_ATTRIBUTES{
             .Length = @sizeOf(std.os.windows.OBJECT_ATTRIBUTES),
+            //.RootDirectory = if (std.fs.path.isAbsoluteWindowsWtf16(path)) null else dir.handle,
             .RootDirectory = dir.handle,
             // TODO: Try std.os.windows.removeDotDirsSanitized and look into proper converion of
             // Win32 paths to NT paths
@@ -168,9 +169,7 @@ const LinuxEntry = std.os.linux.dirent64;
 
 const WindowsEntry = host_os.windows.FILE_ID_FULL_DIR_INFORMATION;
 
-pub const Entry = if (builtin.link_libc and has_dirent)
-    std.c.dirent
-else switch (builtin.os.tag) {
+pub const Entry = switch (builtin.os.tag) {
     .linux => LinuxEntry,
     .windows => WindowsEntry,
     else => |bad| @compileError("no dirent for " ++ @tagName(bad) ++ ", try linking libc"),
@@ -263,9 +262,16 @@ pub const Iterator = struct {
         AccessDenied,
     };
 
-    fn readWindows(iter: *Iterator) Error!?*align(2) const Entry {
-        std.debug.assert(iter.remaining.len == 0);
+    fn peekWindows(iter: *Iterator) Error!?*align(2) const Entry {
+        if (!iter.needs_reset) {
+            if (iter.current()) |peeked_entry| {
+                return peeked_entry;
+            }
+        }
+
         @memset(iter.buffer, undefined);
+        iter.remaining = iter.buffer[0..0];
+
         var io: std.os.windows.IO_STATUS_BLOCK = undefined;
         const status = host_os.windows.queryDirectoryFile(
             iter.dir.handle,
@@ -290,8 +296,7 @@ pub const Iterator = struct {
         };
     }
 
-    fn readPosix(iter: *Iterator) Error!?*align(1) const Entry {
-        std.debug.assert(iter.remaining.len == 0);
+    fn peekPosix(iter: *Iterator) Error!?*align(1) const Entry {
         if (iter.needs_reset) {
             @branchHint(.unlikely);
             std.posix.lseek_SET(iter.dir.handle, 0) catch |e| switch (e) {
@@ -303,8 +308,14 @@ pub const Iterator = struct {
             iter.needs_reset = false;
         }
 
+        if (iter.current()) |peeked_entry| {
+            return peeked_entry;
+        }
+
+        iter.remaining = iter.buffer[0..0];
+        @memset(iter.buffer, 0);
+
         if (builtin.os.tag == .linux) {
-            @memset(iter.buffer, 0);
             const result = std.os.linux.getdents64(
                 iter.dir.handle,
                 iter.buffer.ptr,
@@ -331,15 +342,10 @@ pub const Iterator = struct {
 
     /// May deinitialize any previous `Entry`s that were returned by `.next()`.
     pub fn peek(iter: *Iterator) Error!?*align(entry_align) const Entry {
-        if (iter.current()) |peeked_entry| {
-            return peeked_entry;
-        }
-
-        iter.remaining = iter.buffer[0..0];
         if (host_os.is_windows) {
-            return iter.readWindows();
+            return iter.peekWindows();
         } else {
-            return iter.readPosix();
+            return iter.peekPosix();
         }
     }
 