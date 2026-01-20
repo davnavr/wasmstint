bececf37762221851d3ac191bd3c713c2e206c74
David N <davidcalieln@gmail.com>
2026-01-19 22:08:59 -0800

    chore(x64-asm-simd): implement i8x16.shr_(u|s) handlers

--- src/Interpreter/codegen/x86_64/simd.zig
+++ src/Interpreter/codegen/x86_64/simd.zig
@@ -33,11 +33,13 @@ pub fn defineIntegerOpcodes(as: *AsmWriter) void {
 
     as.write("\n.text\n");
 
-    inline for (&[_]FDPrefixOpcode{
+    for (&[_]FDPrefixOpcode{
         .@"i8x16.shl",
+        .@"i8x16.shr_s",
+        .@"i8x16.shr_u",
     }) |opcode| {
         var op = as.defineOpcodeHandler(.{ .fd = opcode }, .@"64");
-        const interp = @field(IntInterp, @tagName(opcode)[0..5]);
+        const interp = std.meta.stringToEnum(IntInterp, @tagName(opcode)[0..5]).?;
         as.printInstrs(&.{
             "mov r14d, dword ptr [{[vsp]f} - 0x10] # amt to shift",
             "movdqa xmm0, xmmword ptr [{[vsp]f} - 0x20] # vector to shift",
@@ -45,8 +47,8 @@ pub fn defineIntegerOpcodes(as: *AsmWriter) void {
             "movd xmm1, r14d # shift amount in bottom 64 bits",
         }, .{ .vsp = Gpr.vsp, .shift_mask = interp.shiftMask() });
         switch (opcode) {
+            // x86-64 has no packed shift instruction for bytes
             .@"i8x16.shl" => as.printInstrs(&.{
-                // x86-64 has no packed shift instruction for bytes
                 "psllw xmm0, xmm1 # shift values in even lanes",
                 "movdqa xmm2, xmm0",
                 "pand xmm2, xmmword ptr [.L{[symbol_prefix]s}i8x16_even_lanes] # result even lanes",
@@ -56,7 +58,28 @@ pub fn defineIntegerOpcodes(as: *AsmWriter) void {
                 "por xmm0, xmm2",
                 "movdqa xmmword ptr [{[vsp]f} - 0x20], xmm0 # store result",
             }, .{ .symbol_prefix = as.symbol_prefix, .vsp = Gpr.vsp }),
-            else => comptime unreachable,
+            .@"i8x16.shr_s" => as.printInstrs(&.{
+                "movdqa xmm2, xmm0",
+                "psraw xmm0, xmm1 # shift values in odd lanes",
+                "pand xmm0, xmmword ptr [.L{[symbol_prefix]s}i8x16_odd_lanes] # result odd lanes",
+                "psllw xmm2, 8 # move even lanes into odd lanes (low 8-bits to high 8-bits)",
+                "psraw xmm2, xmm1 # shift values in even lanes",
+                "psrlw xmm2, 8 # move even lanes back to their final position",
+                "pand xmm2, xmmword ptr [.L{[symbol_prefix]s}i8x16_even_lanes] # result even lanes",
+                "por xmm0, xmm2",
+                "movdqa xmmword ptr [{[vsp]f} - 0x20], xmm0 # store result",
+            }, .{ .symbol_prefix = as.symbol_prefix, .vsp = Gpr.vsp }),
+            .@"i8x16.shr_u" => as.printInstrs(&.{
+                "psrlw xmm0, xmm1 # shift values in odd lanes",
+                "movdqa xmm2, xmm0",
+                "pand xmm2, xmmword ptr [.L{[symbol_prefix]s}i8x16_odd_lanes] # result odd lanes",
+                "movdqa xmm3, xmmword ptr [.L{[symbol_prefix]s}i8x16_even_lanes]",
+                "psrlw xmm3, xmm1 # mask for even lanes",
+                "pand xmm0, xmm3 # result even lanes",
+                "por xmm0, xmm2",
+                "movdqa xmmword ptr [{[vsp]f} - 0x20], xmm0 # store result",
+            }, .{ .symbol_prefix = as.symbol_prefix, .vsp = Gpr.vsp }),
+            else => unreachable,
         }
         as.printInstrs(&.{"lea {[vsp]f}, [{[vsp]f} - 0x10] # adjust VSP"}, .{ .vsp = Gpr.vsp });
         op.jmpToNextHandler(as);
