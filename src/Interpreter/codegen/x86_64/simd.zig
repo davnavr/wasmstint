1f867d5ffc89fd3372d67f8a5491c319aad78045
David N <davidcalieln@gmail.com>
2026-02-11 00:43:15 -0800

    chore(x64-asm-simd): pminud for i32x4.min_u

--- src/Interpreter/codegen/x86_64/simd.zig
+++ src/Interpreter/codegen/x86_64/simd.zig
@@ -2522,24 +2522,31 @@ fn defineIntegerOpcodes(as: *AsmWriter) void {
         max.end(as);
     }
     {
-        // pminud requires SSE4_1
         var min = as.defineOpcodeHandler(.{ .fd = .@"i32x4.min_u" }, .@"32");
+        if (as.hasFeature(.sse4_1)) {
+            as.printInstrs(&.{
+                "movdqa xmm0, xmmword ptr [{[vsp]f} - 0x20] # operand 0",
+                "pminud xmm0, xmmword ptr [{[vsp]f} - 0x10]",
+            }, .{ .vsp = Gpr.vsp });
+        } else {
+            as.printInstrs(&.{
+                "# Taken from what LLVM emits for Zig's @min builtin",
+                "movdqa xmm1, xmmword ptr [{[vsp]f} - 0x20] # operand 0",
+                "movdqa xmm2, xmmword ptr [{[vsp]f} - 0x10] # operand 1",
+                "movdqa xmm0, xmmword ptr [rip + .L{[symbol_prefix]s}i32x4_sign_bits]",
+                "movdqa xmm3, xmm1",
+                "pxor xmm3, xmm0 # toggles sign bit in operand 0",
+                "pxor xmm0, xmm2 # toggles sign bit in operand 1",
+                "pcmpgtd xmm0, xmm3",
+                "pand xmm1, xmm0",
+                "pandn xmm0, xmm2",
+                "por xmm0, xmm1",
+            }, .{ .vsp = Gpr.vsp, .symbol_prefix = symbol_prefix });
+        }
         as.printInstrs(&.{
-            "# Taken from what LLVM emits for Zig's @min builtin",
-            "movdqa xmm1, xmmword ptr [{[vsp]f} - 0x20] # operand 0",
-            "movdqa xmm2, xmmword ptr [{[vsp]f} - 0x10] # operand 1",
-            "movdqa xmm0, xmmword ptr [rip + .L{[symbol_prefix]s}i32x4_sign_bits]",
-            "movdqa xmm3, xmm1",
-            "pxor xmm3, xmm0 # toggles sign bit in operand 0",
-            "pxor xmm0, xmm2 # toggles sign bit in operand 1",
-            "pcmpgtd xmm0, xmm3",
-            "pand xmm1, xmm0",
-            "pandn xmm0, xmm2",
-            "por xmm0, xmm1",
-
             "movdqa xmmword ptr [{[vsp]f} - 0x20], xmm0 # store result",
             "lea {[vsp]f}, [{[vsp]f} - 0x10] # adjust VSP",
-        }, .{ .vsp = Gpr.vsp, .symbol_prefix = symbol_prefix });
+        }, .{ .vsp = Gpr.vsp });
         min.jmpToNextHandler(as);
         min.end(as);
     }
