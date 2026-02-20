376f16d6a4481153e03e398623be3d30f5f8f01c
David N <davidcalieln@gmail.com>
2026-02-20 04:10:22 -0800

    chore(llvm-ir): integer/float load handlers

--- src/Interpreter/codegen/llvm/main.zig
+++ src/Interpreter/codegen/llvm/main.zig
@@ -1411,7 +1411,7 @@ fn buildMemoryLoadOpcodeHandlers(b: *Builder) Oom!void {
         .{ .sext, .@"4", .i32, "load32_s" },
     };
 
-    for (&[2]Type{ .i32, .i64 }) |int_ty| {
+    for (&[2]Type{ .i32, .i64 }, &[2][]const u8{ "f32", "f64" }) |int_ty, float_name| {
         for (extending_loads[0..(if (int_ty == .i64) 6 else 4)]) |info| {
             const cast, const access_size, const load_ty, const name = info;
             var load = try b.opcodeHandlerFromPrefixedName(ByteOpcode, @tagName(int_ty), name);
@@ -1429,6 +1429,35 @@ fn buildMemoryLoadOpcodeHandlers(b: *Builder) Oom!void {
             _ = try load.wip.store(
                 .normal,
                 loaded_value,
+                try load.operandAt(b, 0),
+                value_stack_alignment,
+            );
+
+            try load.jmpToNextHandler(b, .{
+                .vip = access.vip,
+                .vsp = OpcodeHandlerParam.vsp.arg(&load.wip),
+                .stp = OpcodeHandlerParam.stp.arg(&load.wip),
+            });
+            try load.finish(b);
+        }
+
+        // On ReleaseSmall, f32/f64 memory loads can just be aliases for i32/i64 versions
+        // LLVM might have function deduplication to handle this
+        for ([2][]const u8{ @tagName(int_ty), float_name }) |name| {
+            var load = try b.opcodeHandlerFromPrefixedName(ByteOpcode, name, "load");
+
+            load.wip.cursor = .{ .block = try load.wip.block(0, "Entry") };
+            const perform_load = try load.wip.block(1, "Load");
+            const access = try load.linearMemoryAccess(
+                b,
+                0,
+                .fromByteUnits(@divExact(int_ty.scalarBits(&b.module), 8)),
+                perform_load,
+            );
+
+            _ = try load.wip.store(
+                .normal,
+                try load.wip.load(.normal, int_ty, access.ptr, byte_alignment, ""),
                 try load.operandAt(b, 0),
                 value_stack_alignment,
             );
